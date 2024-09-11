4627adea7ebbd938b29033504bf4319c9e25a6be
Nurhidayah <nurhid180701@gmail.com>
2024-09-11 08:18:24 +0700

    first commit

---
+++ Pertemuan 2/dart_application_1/bin/dart_application_1.dart
@@ -0,0 +1,7 @@
+import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+
+void main() {
+  for (int i = 18 ; i > 8; i--) {
+    print('Nama saya adalah Nurhidayah, sekarang berumur ${i+0}');
+  }
+}
